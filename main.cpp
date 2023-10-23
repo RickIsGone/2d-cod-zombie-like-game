e4e7e696ce6777157c2435632744a3d599434756
RickIsGone <riccardotedeschi247@gmail.com>
2023-10-23 19:19:43 +0200

    modifica

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ const int WIDTH=1920, HEIGHT=1080;
 int main (int argc, char *argv[]){
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *window=SDL_CreateWindow(" zombie",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,WIDTH,HEIGHT,SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window *window=SDL_CreateWindow("2d cod zombie",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,WIDTH,HEIGHT,SDL_WINDOW_ALLOW_HIGHDPI);
 
     if(NULL==window){
         std::cout<<"error"<<SDL_GetError()<<std::endl;
