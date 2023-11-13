32d23e7e4a9f957014bd177c76f0e74090670fd7
Rick <143021811+RickIsGone@users.noreply.github.com>
2023-11-13 09:43:32 +0100

    Update g_variables.hpp

--- hpp files/g_variables.hpp
+++ hpp files/g_variables.hpp
@@ -11,4 +11,7 @@ enum GameState{
 bool start;
 const string game_map;
 
+Mix_Chunk *step,*ak47_fire,*mp5_fire,*glock18_fire,*reload,*empty_mag;
+SDL_Texture* wall_texture,*ground_texture,*outside_texture,*glock18_outline_texture,*ak_outline_texture,*mp5_outline_texture,*zombie_texture;
+
 #endif
