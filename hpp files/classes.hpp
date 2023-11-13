ba713a2c21543e65748edf1f4d493ad9eeb7098c
Rick <143021811+RickIsGone@users.noreply.github.com>
2023-11-13 09:34:32 +0100

    Update classes.hpp

--- hpp files/classes.hpp
+++ hpp files/classes.hpp
@@ -1,4 +1,48 @@
 #ifndef CLASSES
 #define CLASSES
 
+struct MouseState {
+    int x, y; 
+    bool leftButton, rightButton; 
+
+    MouseState(int x, int y, bool leftButton, bool rightButton) 
+        : x(x), y(y), leftButton(leftButton), rightButton(rightButton) {}
+};
+
+
+struct gun {
+    std::string name;
+    int damage;
+    int ammo;
+    int ammo_max;
+    int ammo_left;
+    int fire_rate;
+    void shoot(bool automatic);
+    
+};
+
+struct players {
+    int health;
+    int health_max;
+    float speed;
+    gun WeaponInHand;
+    gun WeaponInInventory;
+    int money;
+};
+
+struct rounds {
+    int round_number;
+    int zombie_number;
+    int zombie_left;
+};
+
+extern gun ak47;
+extern gun mp5;
+extern gun knife;
+extern gun glock18;
+
+
+extern rounds game_round;
+extern players player;
+
 #endif
