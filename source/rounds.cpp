2d56322ec0aae2e3058442f0e930f981d24ce087
Rick <143021811+RickIsGone@users.noreply.github.com>
2023-11-13 08:17:08 +0100

    Update rounds.cpp

--- source/rounds.cpp
+++ source/rounds.cpp
@@ -10,29 +10,29 @@ gun knife{"knife",85,1,1,1,200};
 gun glock18{"glock18",20,20,20,160,0};
 players player={150,150,4,glock18,knife,0};
 
-void start_game(){
+void game::start(){
 
     game_round.round_number=1;
     game_round.zombie_number=10;
 
-    spawn_zombie();
+    game::spawn_zombie();
 }
 
 static Uint32 s_RoundWon = 0;
 static const Uint32 s_ZombieDelay = 10000;
 
-void won_round(){
+void game::won_round(){
     game_round.round_number++;
     game_round.zombie_number+=2;
     
     s_RoundWon=SDL_GetTicks();
-    if(s_RoundWon!=0 && SDL_GetTicks()-s_RoundWon>=s_ZombieDelay) spawn_zombie();
+    if(s_RoundWon!=0 && SDL_GetTicks()-s_RoundWon>=s_ZombieDelay) game::spawn_zombie();
     
 }
 
-void death(int &game_state,SDL_Rect &camera,MouseState &mouseState){
+void game::death(int &game_state,SDL_Rect &camera,MouseState &mouseState){
     camera={500,300,1920,1080};     
     mouseState = {0, 0, false, false};
     player={150,150,4,glock18,knife,0};
-    start_game();
+    game::start();
 }
