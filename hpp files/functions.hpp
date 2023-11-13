18534729ae7ef6988256603c81da05dabb434485
Rick <143021811+RickIsGone@users.noreply.github.com>
2023-11-13 09:33:31 +0100

    Update functions.hpp

--- hpp files/functions.hpp
+++ hpp files/functions.hpp
@@ -1,4 +1,25 @@
 #ifndef FUNCTIONS
 #define FUNCTIONS
 
+void map(SDL_Renderer* renderer,const SDL_Rect &camera);
+void mnk_events(SDL_Rect &camera, MouseState &mouseState, int &game_state,SDL_Event event,SDL_Renderer* renderer,bool &no_clip);
+void events(SDL_Event event, int &game_state);
+void niga(SDL_Renderer* renderer,MouseState mouseState);
+void mouse(SDL_Renderer* renderer,MouseState mouseState);
+
+void menu(SDL_Renderer* renderer);
+void pause(int &game_state,SDL_Event event);
+void pause_menu(SDL_Rect &camera, int &game_state,SDL_Event event,SDL_Renderer* renderer);
+void death_menu(SDL_Renderer* renderer);
+
+void round_display(SDL_Renderer* renderer,SDL_Rect camera);
+void ammo_display(SDL_Renderer* renderer,SDL_Rect camera);
+void health_display(SDL_Renderer* renderer,SDL_Rect camera);
+void money_display(SDL_Renderer* renderer,SDL_Rect camera);
+void hud_display(SDL_Renderer* renderer,SDL_Rect camera);
+
+void consolle(bool &no_clip,SDL_Rect &camera,SDL_Renderer* renderer,MouseState mouseState,int &game_state);
+void consolle_events(SDL_Event consolle_events, bool &consolle_state);
+void death_events(SDL_Event event,int &game_state);
+
 #endif
